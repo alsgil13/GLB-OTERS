e7f6fec688ef6705c58dabb8ef4c03756cbe4e44
alsgil13 <als.gil13@gmail.com>
2022-08-08 11:22:48 -0300

    Cria teste para ver como dados saem do banco

--- backend/teste.php
+++ backend/teste.php
@@ -36,8 +36,8 @@ echo "<br>Total grupo 1: $totalG1";
 
 
 //Fazer select para grupo 2
-$select_grupo1 = "SELECT seqcod, COUNT(seqcod) as tanto FROM lpactm_data_2 WHERE grupo = 2 GROUP BY seqcod ORDER BY tanto ASC;";
-$stmt= $conn->prepare($select_grupo1);
+$select_grupo2 = "SELECT seqcod, COUNT(seqcod) as tanto FROM lpactm_data_2 WHERE grupo = 2 GROUP BY seqcod ORDER BY tanto ASC;";
+$stmt= $conn->prepare($select_grupo2);
 $stmt->execute();
 $grupo2seqs = [];
 $totalG2 = 0;
@@ -48,16 +48,16 @@ while($d = $stmt->fetch(PDO::FETCH_ASSOC)){
     
     switch($d['seqcod']){
         case 1:
-            $grupo1seqs['1'] = (int)$d['tanto'];
-            $totalG1 += $grupo1seqs['1'];
+            $grupo2seqs['1'] = (int)$d['tanto'];
+            $totalG1 += $grupo2seqs['1'];
             break;
         case 2:
-            $grupo1seqs['2'] = (int)$d['tanto']; 
-            $totalG1 += $grupo1seqs['2'];
+            $grupo2seqs['2'] = (int)$d['tanto']; 
+            $totalG1 += $grupo2seqs['2'];
             break;
         case 3:
-            $grupo1seqs['3'] = (int)$d['tanto'];
-            $totalG1 += $grupo1seqs['3'];
+            $grupo2seqs['3'] = (int)$d['tanto'];
+            $totalG1 += $grupo2seqs['3'];
             break;
     }
 }
@@ -67,8 +67,8 @@ echo "</pre>";
 echo "<br>Total grupo 2: $totalG2";
 
 //Fazer select para grupo 3
-$select_grupo1 = "SELECT COUNT(grupo) as tanto FROM lpactm_data_2 WHERE grupo = 3;";
-$stmt= $conn->prepare($select_grupo1);
+$select_grupo3 = "SELECT COUNT(grupo) as tanto FROM lpactm_data_2 WHERE grupo = 3;";
+$stmt= $conn->prepare($select_grupo3);
 $stmt->execute();
 $totalG3 = 0;
 while($d = $stmt->fetch(PDO::FETCH_ASSOC)){
