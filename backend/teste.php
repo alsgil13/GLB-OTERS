d808788fca8c521ebd6cdcaf85066ab255ba1ce3
alsgil13 <als.gil13@gmail.com>
2022-08-08 11:17:13 -0300

    Cria teste para ver como dados saem do banco

--- backend/teste.php
+++ backend/teste.php
@@ -6,9 +6,28 @@ require_once "../db/connect.php";
 $select_grupo1 = "SELECT seqcod, COUNT(seqcod) as tanto FROM lpactm_data_2 WHERE grupo = 1 GROUP BY seqcod ORDER BY tanto ASC;";
 $stmt= $conn->prepare($select_grupo1);
 $stmt->execute();
-$dados = [];
+$grupo1seqs = [];
+$totalG1 = 0;
 while($d = $stmt->fetch(PDO::FETCH_ASSOC)){
-    array_push($dados, $d);
+    //array_push($dados, $d);
+
+    //coleta a quantidade de sequências do grupo 1
+    
+    switch($d['seqcod']){
+        case 1:
+            $grupo1seqs['1'] = $d['tanto'];
+            $totalG1 += $grupo1seqs['1'];
+            break;
+        case 2:
+            $grupo1seqs['2'] = $d['tanto']; 
+            $totalG1 += $grupo1seqs['2'];
+            break;
+        case 3:
+            $grupo1seqs['3'] = $d['tanto'];
+            $totalG1 += $grupo1seqs['3'];
+            break;
+    }
 }
 echo "<pre>";
-var_dump($dados);
+var_dump($grupo1seqs);
+echo "<br>$totalG1";
