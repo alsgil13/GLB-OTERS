b98c2b68879c5c104f3c95a3784a449f899ebddc
alsgil13 <als.gil13@gmail.com>
2022-08-08 16:04:03 -0300

    Define esquema de escolha de grupos e sequências

--- backend/insereParticipante.php
+++ backend/insereParticipante.php
@@ -22,56 +22,142 @@ if(isset($_COOKIE["dispositivo"])){
     $msg .= "\nDispositivo não encontrado nos arquivos do navegador";
 }
 
-$insert = "INSERT INTO lpactm_data_2 
-(nome, email, dispositivo) VALUES ('$nome','$email','$dispositivo');";
 
-$stmt= $conn->prepare($insert);
-$stmt->execute();
 
-echo $msg;
+// echo $msg;
 
 //Define grupos e sequência
-
-
-//Fazer select para grupo 1 seq 1
 $select_grupo1 = "SELECT seqcod, COUNT(seqcod) as tanto FROM lpactm_data_2 WHERE grupo = 1 GROUP BY seqcod ORDER BY tanto ASC;";
 $stmt= $conn->prepare($select_grupo1);
 $stmt->execute();
-$dados = [];
+$grupo1seqs = [];
+$totalG1 = 0;
 while($d = $stmt->fetch(PDO::FETCH_ASSOC)){
-    array_push($dados, $d);
+    //array_push($dados, $d);
+
+    //coleta a quantidade de sequências do grupo 1
+    
+    switch($d['seqcod']){
+        case 1:
+            $grupo1seqs['1'] = (int)$d['tanto'];
+            $totalG1 += $grupo1seqs['1'];
+            break;
+        case 2:
+            $grupo1seqs['2'] = (int)$d['tanto']; 
+            $totalG1 += $grupo1seqs['2'];
+            break;
+        case 3:
+            $grupo1seqs['3'] = (int)$d['tanto'];
+            $totalG1 += $grupo1seqs['3'];
+            break;
+    }
 }
-echo "<pre>";
-var_dump($dados);
 
-//Fazer select para grupo 1 seq 2
+//Fazer select para grupo 2
+$select_grupo2 = "SELECT seqcod, COUNT(seqcod) as tanto FROM lpactm_data_2 WHERE grupo = 2 GROUP BY seqcod ORDER BY tanto ASC;";
+$stmt= $conn->prepare($select_grupo2);
+$stmt->execute();
+$grupo2seqs = [];
+$totalG2 = 0;
+while($d = $stmt->fetch(PDO::FETCH_ASSOC)){
+    //array_push($dados, $d);
 
-//Fazer select para grupo 1 seq 3
+    //coleta a quantidade de sequências do grupo 2
+    
+    switch($d['seqcod']){
+        case 1:
+            $grupo2seqs['1'] = (int)$d['tanto'];
+            $totalG2 += $grupo2seqs['1'];
+            break;
+        case 2:
+            $grupo2seqs['2'] = (int)$d['tanto']; 
+            $totalG2 += $grupo2seqs['2'];
+            break;
+        case 3:
+            $grupo2seqs['3'] = (int)$d['tanto'];
+            $totalG2 += $grupo2seqs['3'];
+            break;
+    }
+}
+
+
+//Fazer select para grupo 3
+$select_grupo3 = "SELECT COUNT(grupo) as tanto FROM lpactm_data_2 WHERE grupo = 3;";
+$stmt= $conn->prepare($select_grupo3);
+$stmt->execute();
+$totalG3 = 0;
+while($d = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $totalG3 = (int)$d['tanto'];
+}
 
 
 
+//define os grupos
 
-//Fazer select para grupo 2 seq 1
+$totais = [
+    "1" => $totalG1,
+    "2" => $totalG2,
+    "3" => $totalG3
 
-//Fazer select para grupo 2 seq 2
-
-//Fazer select para grupo 2 seq 3
+];
 
 
-//Fazer select para grupo 3 seq 1
+asort($totais);
 
-// $select_grupo = "SELECT grupo, COUNT(grupo) as tanto FROM lpactm_data_2 GROUP BY grupo ORDER BY tanto ASC;";
-// $stmt= $conn->prepare($select);
-// $stmt->execute();
-// //$dados = [];
-// while($d = $stmt->fetch(PDO::FETCH_ASSOC)){
-//     $grupo = (int)$d['grupo'];
-//     break;
-// }
+foreach($totais as $chave => $valor){
+    $grupo = (int)$chave;
+    break;
+}
 
-// if($grupo < 3){
-//     //define sequência
-// } else {
-//     //Grupo 3 só tem uma sequência
-//     $seq = 1;
-// }
+
+//Define a sequência
+if($grupo == 3){
+    $seqcod = 1;
+    $sequencia = "['a','b','c']";
+} else {
+    //Buscar sequência
+    if($grupo == 1){
+        if(!isset($grupo1seqs[1])){
+            $seqcod = 1;
+        } elseif(!isset($grupo1seqs[2])){
+            $seqcod = 2;
+        } elseif(!isset($grupo1seqs[3])){
+            $seqcod = 3;
+        } else{
+            asort($grupo1seqs);
+            foreach($grupo1seqs as $chave => $valor){
+                $seqcod = (int)$chave;
+                break;
+            }
+        }
+    } elseif($grupo == 2){
+        if(!isset($grupo2seqs[1])){
+            $seqcod = 1;
+        } elseif(!isset($grupo2seqs[2])){
+            $seqcod = 2;
+        } elseif(!isset($grupo2seqs[3])){
+            $seqcod = 3;
+        } else{
+            asort($grupo2seqs);
+            foreach($grupo2seqs as $chave => $valor){
+                $seqcod = (int)$chave;
+                break;
+            }
+        }
+    }
+
+}
+
+$array_sequencias = [
+    1 => "['a','b','c']",
+    2 => "['c','b','a']",
+    3 => "['a','c','b']"
+];
+
+$sequencia = $array_sequencias[$seqcod];
+
+$insert = "INSERT INTO lpactm_data_2 
+(nome, email, dispositivo, grupo, seqcod, sequencia) VALUES ('$nome', '$email', '$dispositivo', $grupo, $seqcod, '$sequencia');";
+
+$stmt= $conn->prepare($insert);
+$stmt->execute();
