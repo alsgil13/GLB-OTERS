a8961170c1cb0d95655f3741bb7e4bf2dafcd9da
alsgil13 <als.gil13@gmail.com>
2022-08-10 15:28:59 -0300

    Cria telas de treino com Áudio

--- backend/telaespera.php
+++ backend/telaespera.php
@@ -178,7 +178,7 @@ $stmt->execute();
     
 </div>
 
-<script src="assets/js/validaForms.js?v=2"></script>
+<script src="../assets/js/validaForms.js?v=2"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
 </body>
