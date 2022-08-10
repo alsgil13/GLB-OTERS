e3e7f3b4feddaa256358ddbc9d83bca5c59c3a60
alsgil13 <als.gil13@gmail.com>
2022-08-10 07:32:03 -0300

    Cria arquivo de funções auxiliares em php

--- backend/funcoes.inc.php
+++ backend/funcoes.inc.php
@@ -3,7 +3,7 @@
 function arrayToString($array){
     $string = '';
     foreach($array as $a){
-        $string = '"'.$a."',";
+        $string = '"'.$a.'",';
     }
     //$string = substr($string,-1,1);
     return $string;
