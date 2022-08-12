bbca9957a18e5969e68712669a53cf4f7b627cda
alsgil13 <als.gil13@gmail.com>
2022-08-12 11:23:21 -0300

    Cria testes Imediatos

--- backend/finalizaTsI.php
+++ backend/finalizaTsI.php
@@ -24,8 +24,8 @@ $lista_de_biscoitos = [
 
 //Limpa cookies
 for($i=1;$i<=13;$i++){
-    $nome_cok_i = "ini_test_audio_" + $i;
-    $nome_cok_f = "fim_test_audio_" + $i;
+    $nome_cok_i = "ini_test_audio_" . $i;
+    $nome_cok_f = "fim_test_audio_" . $i;
     setcookie($nome_cok_i, "", time() - 3600, '/LPACTM2');
     setcookie($nome_cok_f, "", time() - 3600, '/LPACTM2');
 }
